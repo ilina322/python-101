e60e63292992f95bbaf3887aadad43d478ad290b
ilina322 <ilina.pavllova@gmail.com>
2019-04-08 14:21:09 +0300

    update queries

--- week_04/queries/queries.py
+++ week_04/queries/queries.py
@@ -13,36 +13,57 @@ def parse_csv(filename):
             people.append(person)
     return people
 
-def filter(filename, **kwargs):
-    with open(filename) as data:
-        reader = csv.reader(data)
-        keys = next(reader)
+def check_requirements(person, requirements):
+    passes = True
+    for key, value in requirements.items():
+        if '__' in key:
+            args = key.split('__')
+            if args[1] == 'startswith':
+                if not person[args[0]].startswith(value):
+                    passes = False
+            elif args[1] == 'contains':
+                if not value in person[args[0]]:
+                    passes = False
+        else:
+            if person[key] != value:
+                passes = False
+    return passes
 
+def filter(filename, **kwargs):
     people = parse_csv(filename)
     filtered_people = []
 
     for person in people:
-        passes = True
-        for key, value in kwargs.items():
-            if '__' in key:
-                args = key.split('__')
-                if args[1] == 'startswith':
-                    if not person[args[0]].startswith(value):
-                        passes = False
-                elif args[1] == 'contains':
-                    if not value in person[args[0]]:
-                        passes = False
-                else:
-                    if person[key] != value:
-                        passes = False
+        passes = check_requirements(person, kwargs)
         if passes:
             filtered_people.append(person)
 
     return filtered_people
 
+def count(filename, **kwargs):
+    people = parse_csv(filename)
+    people_count = 0
+
+    for person in people:
+        passes = check_requirements(person, kwargs)
+        if passes:
+            people_count += 1
+
+    return people_count
+
+def first(filename, **kwargs):
+    people = parse_csv(filename)
+
+    for person in people:
+        passes = check_requirements(person, kwargs)
+        if passes:
+            return person
+
+    return None
+
 
 def main():
-    print(filter('example_data.csv', full_name__startswith='Mi'))
+    print(filter('example_data.csv', favourite_color='olive'))
 
 if __name__ == '__main__':
     main()
